9e991b07a9648112259ff6215d2747642371c65b
animesh <kanimesh.s@gmail.com>
2016-05-11 18:09:45 +0530

    Refactoring, request methods added, can add new projects

--- resources/ui/ui.js
+++ resources/ui/ui.js
@@ -1,8 +1,9 @@
 const DataServer = "http://localhost:1234/";
+var currentProject = null;
 function get(url) {
   return $.ajax({
     type: "get",
-    url : DataServer + url,
+    url: DataServer + url,
     contentType: "application/json"
   });
 }
@@ -12,35 +13,98 @@ function post(url, data, config) {
     data: JSON.stringify(data),
     contentType: "application/json",
     type: "post"
-  }).done(function(resp) {
+  }).done(function (resp) {
     console.log("response is", resp);
-  }).fail(function(resp) {
+  }).fail(function (resp) {
     if (resp.status && resp.statusText) {
       console.error(resp.status + " : " + resp.statusText, "error");
     }
   });
 }
-function ApiListItemHTML(data){
+function ApiListItemHTML(data) {
+  var reqType = data.type;
   return (
-    '<form class="api" id="'+ data._id +'">' +
-      '<select name="type">' +
-        (data.type === 'GET' ? '<option selected value="GET">GET</option>': '<option value="GET">GET</option>' ) +
-        (data.type === 'POST' ? '<option selected value="POST">POST</option>': '<option value="POST">POST</option>' ) +
-      '</select>' +
-      '<input type="text" name="apiname" value="'+ data.apiname +'"/>' +
-      '<textarea name="req" rows="10">' + JSON.stringify(data.req, undefined, 3) + '</textarea>' +
-      '<textarea name="resp" rows="10">' + JSON.stringify(data.resp, undefined, 3) + '</textarea>'+
-      '<button class="edit-api" edit-id="'+ data._id +'">edit</button>'+
-      '<button class="delete-api" delete-id="'+ data._id +'">delete</button>'+
-      '<input type="text" name="syncURI" value="http://10.41.92.108:8080"/>' +
-      '<button class="sync-api" sync-id="'+ data._id +'">sync</button>'+
+    '<form class="api" id="' + data._id + '">' +
+    '<select name="type">' +
+      '<option value="GET"'+ (reqType === 'GET' ? 'selected' : '') +'> GET</option>' +
+      '<option value="POST"'+ (reqType === 'POST' ? 'selected' : '') +'> POST</option>' +
+      '<option value="PUT"'+ (reqType === 'PUT' ? 'selected' : '') +'> PUT</option>' +
+      '<option value="DELETE"'+ (reqType === 'DELETE' ? 'selected' : '') +'> DELETE</option>' +
+      '<option value="PATCH"'+ (reqType === 'PATCH' ? 'selected' : '') +'>PATCH</option>'+
+      '<option value="COPY"'+ (reqType === 'COPY' ? 'selected' : '') +'>COPY</option>'+
+      '<option value="HEAD"'+ (reqType === 'HEAD' ? 'selected' : '') +'>HEAD</option>'+
+      '<option value="OPTIONS"'+ (reqType === 'OPTIONS' ? 'selected' : '') +'>OPTIONS</option>'+
+      '<option value="LINK"'+ (reqType === 'LINK' ? 'selected' : '') +'>LINK</option>'+
+      '<option value="UNLINK"'+ (reqType === 'UNLINK' ? 'selected' : '') +'>UNLINK</option>'+
+      '<option value="PURGE"'+ (reqType === 'PURGE' ? 'selected' : '') +'>PURGE</option>'+
+    '</select>' +
+    '<input type="text" name="apiname" value="' + data.apiname + '"/>' +
+    '<textarea name="req" rows="10">' + JSON.stringify(data.req, undefined, 3) + '</textarea>' +
+    '<textarea name="resp" rows="10">' + JSON.stringify(data.resp, undefined, 3) + '</textarea>' +
+    '<button class="edit-api" edit-id="' + data._id + '">edit</button>' +
+    '<button class="delete-api" delete-id="' + data._id + '">delete</button>' +
+    '<input type="text" name="syncURI" value="http://10.41.92.108:8080"/>' +
+    '<button class="sync-api" sync-id="' + data._id + '">sync</button>' +
     '</form>'
   );
 }
-function getApiPostData(formData){
+function renderProjectList(force) {
+  if (force || document.getElementById("projects").options.length === 0) {
+    return get("projects").done(function (resp) {
+      var selectProjectOptionsHtml = "";
+      resp.projects.map(function (project) {
+        selectProjectOptionsHtml = selectProjectOptionsHtml + '<option value="' + project.name + '">' + project.name + '</option>';
+      });
+      $("select#projects").html(selectProjectOptionsHtml);
+    });
+  } else {
+    return $.when();
+  }
+}
+function addProject(e) {
+  e.preventDefault();
+  var newProjectName = $("input#new-project-input").val();
+  post("project/add/" + newProjectName).done(function (resp) {
+    if (resp.success) {
+      console.log("project added!");
+      $('#projects').append($('<option>', {
+        value: newProjectName,
+        text: newProjectName
+      }));
+    }
+  });
+}
+function deleteProject(e) {
+  e.preventDefault();
+  var $thisButton = $(e.target);
+  if ($thisButton.attr("confirmed") === undefined) {
+    $thisButton
+      .attr("confirmed", "yes")
+      .text("sure?");
+    window.setTimeout(function () {
+      $thisButton
+        .removeAttr("confirmed")
+        .text("Delete this Project")
+        .prop('disabled', false);
+    }, 1500)
+  } else if ($thisButton.attr("confirmed") === "yes") {
+    $thisButton.prop('disabled', true);
+    post("project/remove/" + currentProject).done(function (resp) {
+      if (resp.success) {
+        console.log("project removed!!", resp);
+        renderCurrentApis(true);
+      }
+    })
+  }
+}
+function changeProject() {
+  currentProject = this.value;
+  renderCurrentApis();
+}
+function getApiPostData(formData) {
   var postData = {};
-  formData.map(function(item, index){
-    if(item.name === "apiname" || item.name === "type" || item.name === "syncURI"){
+  formData.map(function (item, index) {
+    if (item.name === "apiname" || item.name === "type" || item.name === "syncURI") {
       postData[item.name] = item.value;
     } else {
       postData[item.name] = JSON.parse(item.value);
@@ -48,40 +112,40 @@ function getApiPostData(formData){
   });
   return postData;
 }
-function addApi(e){
+function addApi(e) {
   e.preventDefault();
   var formData = jQuery("form#api-input-container").serializeArray();
   var postData = getApiPostData(formData);
-  post("api/add", postData).done(function(resp){
-    if(resp && resp.success){
+  post(currentProject + "/api/add", postData).done(function (resp) {
+    if (resp && resp.success) {
       console.log("api added! :)");
       renderCurrentApis();
     }
   })
 }
-function deleteApi(e,target,data){
-  e.preventDefault();
-  var idToDelete = $(e.target).attr('delete-id');
-  post("api/delete/"+ idToDelete).done(function(resp){
-    console.log(idToDelete + " deleted! :)");
-    renderCurrentApis();
-  })
-}
-function editApi(e,target,data){
+function editApi(e, target, data) {
   e.preventDefault();
   var idToEdit = $(e.target).attr('edit-id');
-  var formData = jQuery("form#"+ idToEdit).serializeArray();
+  var formData = jQuery("form#" + idToEdit).serializeArray();
   var postData = getApiPostData(formData);
-  post("api/edit/"+idToEdit, postData).done(function(resp){
-    if(resp && resp.success){
+  post(currentProject + "/api/edit/" + idToEdit, postData).done(function (resp) {
+    if (resp && resp.success) {
       console.log("api updated! :)");
     }
   });
 }
-function syncApi(e){
+function deleteApi(e, target, data) {
+  e.preventDefault();
+  var idToDelete = $(e.target).attr('delete-id');
+  post(currentProject + "/api/delete/" + idToDelete).done(function (resp) {
+    console.log(idToDelete + " deleted! :)");
+    renderCurrentApis();
+  })
+}
+function syncApi(e) {
   e.preventDefault();
   var idToSync = $(e.target).attr('sync-id');
-  var $thisApi = $("form#"+idToSync);
+  var $thisApi = $("form#" + idToSync);
   var apiData = getApiPostData($thisApi.serializeArray());
   var syncServer = apiData.syncURI;
   var apiURI = apiData.apiname;
@@ -91,31 +155,37 @@ function syncApi(e){
     'uri': syncServer + apiURI,
     'type': apiType,
     'postData': postData
-  }).done(function(resp){
+  }).done(function (resp) {
     $thisApi.find("[name='resp']").val(JSON.stringify(resp, undefined, 3));
   });
 }
-function renderCurrentApis(){
+function renderCurrentApis(refreshProjectList) {
   $(".delete-api, .edit-api, .sync-api").unbind();
-  get("apis").done(function(resp){
-    console.log("hi", resp);
-    var apisHTML = "";
-    resp.apis.map(function(api, index){
-      apisHTML = apisHTML + ApiListItemHTML(api);
-    });
-    $("api-container").html(apisHTML);
 
-    if(resp.apis.length !== 0){
-      $(".delete-api").on("click", deleteApi);
-      $(".edit-api").on("click", editApi);
-      $(".sync-api").on("click", syncApi);
-    }
+  renderProjectList(refreshProjectList).done(function () {
+    currentProject = $("select#projects").val();
+    get(currentProject + "/apis").done(function (resp) {
+      var apisHTML = "";
+      resp.apis.map(function (api, index) {
+        apisHTML = apisHTML + ApiListItemHTML(api);
+      });
+      $("api-container").html(apisHTML);
+
+      if (resp.apis.length !== 0) {
+        $(".delete-api").on("click", deleteApi);
+        $(".edit-api").on("click", editApi);
+        $(".sync-api").on("click", syncApi);
+      }
+    });
   });
 }
 
 $(
-  function(){
+  function () {
     $("#add-api").on("click", addApi);
+    $("#add-project").on("click", addProject);
+    $("#delete-project").on("click", deleteProject);
+    $("#projects").on("change", changeProject);
     renderCurrentApis();
   }
 );
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var ObjectId = require('mongodb').ObjectID;
 var bodyParser = require('body-parser');
 var request = require('request');
 var access_token = null;
-var refresh_token = null;
 
 var server;
 var mongoDBInstance;
@@ -14,13 +13,12 @@ var mongoDBInstance;
 const PORT = 1234;
 var app = express();
 
-// var allowCrossDomain = function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', 'example.com');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// };
-//
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
 // app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/resources'));
 app.use(bodyParser.json());       // to support JSON-encoded bodies
@@ -28,105 +26,143 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
-function getAccessToken(callback){
-  if(access_token === null){
+function getAccessToken(callback) {
+  if (access_token === null) {
     request.post(
       "http://10.41.92.108:9005/oauth/token?grant_type=password&client_id=my-trusted-client&username=akshay.agarwal@unicommerce.com&password=uniware",
       {},
       function (error, response, body) {
-        // console.log("status: ", response.statusCode, "body", body);
-        console.log("type of login response",typeof body);
         access_token = (JSON.parse(body)).access_token;
-        if(callback && typeof callback === "function"){
+        if (callback && typeof callback === "function") {
           callback();
         }
       }
     );
   } else {
-    if(callback && typeof callback === "function"){
+    if (callback && typeof callback === "function") {
       callback();
     }
   }
 }
 
-
-app.get("/", function (req, res) {
+//functionality endpoints
+function getIndex(req, res) {
   res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/apis', function (req, res) {
-  mongoDBInstance.collection('apis').find().toArray(function (err, result) {
+}
+function getProjects(req, res) {
+  //http://stackoverflow.com/a/30647605/3248247
+  mongoDBInstance.listCollections().toArray(function (err, result) {
     if (err) {
-      console.log(err);
+      res.send({'error': err});
+    } else if (result.length) {
+      var projects = result.filter(function (item) {
+        return (item.name !== "system.indexes");
+      });
+      var final = {
+        "projects": projects
+      };
+      res.json(final);
+    } else {
+      res.send({'error': 'No document(s) found with defined "find" criteria!'});
+    }
+  });
+}
+function addProject(req, res) {
+  var name = req.params.name;
+  mongoDBInstance.createCollection(name, null, function (err) {
+    res.send({"success": true, "err": err});
+  });
+
+}
+function removeProject(req, res) {
+  var name = req.params.name;
+  mongoDBInstance.collection(name).drop(function (err) {
+    res.json({"success": true, "error": err});
+  })
+}
+function getApis(req, res) {
+  var project = req.params.project;
+  mongoDBInstance.collection(project).find().toArray(function (err, result) {
+    if (err) {
+      res.json({"error": err});
     } else if (result.length) {
       var final = {
         "apis": result
       };
-      console.log('Found:', result);
+      res.json(final);
     } else {
-      console.log('No document(s) found with defined "find" criteria!');
+      res.json({
+        "apis": []
+      });
     }
-    res.json(final);
   });
-});
-
-app.post('/api/add', function (req, res) {
+}
+function addApi(req, res) {
+  var project = req.params.project;
   var row = req.body;
   if (row && row.apiname) {
-    mongoDBInstance.collection('apis').insertOne(row);
+    mongoDBInstance.collection(project).insertOne(row);
     res.json({'success': true});
   } else {
     res.json({'success': false});
   }
-});
-
-app.get('/yo', function (req, res) {
-  getAccessToken(function(){
-    console.log("accesstoken", access_token);
-    request(
-      {
-        url: 'http://10.41.92.108:8080/batch',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': access_token
-        }
-      },
-      function (error, response, body) {
-        // console.log("status: ", response.statusCode, "body", body);
-        res.json(JSON.parse(body));
-      }
-    );
+}
+function editApi(req, res) {
+  var project = req.params.project;
+  var rowId = req.params.id;
+  var newRow = req.body;
+  if (newRow && newRow.apiname) {
+    mongoDBInstance.collection(project).update({
+      '_id': ObjectId(rowId)
+    }, newRow);
+    res.json({'success': true});
+  } else {
+    res.json({'success': false});
+  }
+}
+function deleteApi(req, res) {
+  var project = req.params.project;
+  var rowId = req.params.id;
+  mongoDBInstance.collection(project).remove({
+    "_id": ObjectId(rowId)
   });
-
-});
-
-app.post('/api/sync', function(req, res){
+  res.json({'success': true});
+}
+function syncApi(req, res) {
   var syncData = req.body;
-  console.log("type of sync post data", typeof syncData.postData);
-  switch(syncData.type){
+  switch (syncData.type) {
     case 'GET':
-      getAccessToken(function(){
+    case 'DELETE':
+    case 'COPY':
+    case 'LINK':
+    case 'UNLINK':
+      getAccessToken(function () {
         request(
           {
             url: syncData.uri,
+            method: syncData.type,
             headers: {
               'Content-Type': 'application/json',
               'Authorization': access_token
             }
           },
           function (error, response, body) {
-            // console.log("status: ", response.statusCode, "body", body);
             res.json(JSON.parse(body));
           }
         );
       });
       break;
     case 'POST':
-      getAccessToken(function(){
+    case 'PUT':
+    case 'PATCH':
+    case 'HEAD':
+    case 'OPTIONS':
+    case 'PURGE':
+      getAccessToken(function () {
         request(
           {
             url: syncData.uri,
-            method: 'POST',
+            method: syncData.type,
             headers: {
               'Content-Type': 'application/json',
               'Authorization': access_token
@@ -134,73 +170,53 @@ app.post('/api/sync', function(req, res){
             json: syncData.postData
           },
           function (error, response, body) {
-            // console.log("status: ", response.statusCode, "body", body);
-            console.log("error", error, "body", body);
+            console.log("GETTING ACCESS TOKEN, error: ", error, "body: ", body);
             res.json(body);
           }
         );
       });
       break;
   }
-});
-
-app.post('/api/edit/:id', function (req, res) {
-  var rowId = req.params.id;
-  var newRow = req.body;
-  if (newRow && newRow.apiname) {
-    mongoDBInstance.collection('apis').update({
-      '_id': ObjectId(rowId)
-    }, newRow);
-    res.json({'success': true});
-  } else {
-    res.json({'success': false});
-  }
-});
-
-app.post('/api/delete/:id', function (req, res) {
-  var rowId = req.params.id;
-  mongoDBInstance.collection('apis').remove({
-    "_id": ObjectId(rowId)
-  });
-  res.json({'success': true});
-});
-
-app.get('/*', function (req, res) {
+}
+function simpleResponse(req, res) {
+  var project = req.params.project;
   console.log(req.originalUrl);
-  // var queryUrl = req.originalUrl.slice(4);
-  var queryUrl = req.originalUrl;
-  var queriedRecord = mongoDBInstance.collection('apis').find({'apiname': queryUrl});
+  var queryUrl = "/" + req.originalUrl.split("/").splice(2).join("/");
+  var queriedRecord = mongoDBInstance.collection(project).find({'apiname': queryUrl});
   queriedRecord.toArray(function (err, result) {
     if (err) {
-      console.log(err);
-      res.send();
+      res.send({"error": err});
     } else if (result.length) {
-      console.log('Found:', result);
       res.json(result[0].resp);
     } else {
-      console.log('No document(s) found with defined "find" criteria!');
-      res.send();
+      res.send({"message":"No document(s) found with defined 'find' criteria!" });
     }
   });
-});
+}
 
-app.post('/*', function (req, res) {
-  console.log(req.originalUrl);
-  var queryUrl = req.originalUrl;
-  var queriedRecord = mongoDBInstance.collection('apis').find({'apiname': queryUrl});
-  queriedRecord.toArray(function (err, result) {
-    if (err) {
-      console.log(err);
-      res.send();
-    } else if (result.length) {
-      console.log('Found:', result);
-      res.json(result[0].resp);
-    } else {
-      console.log('No document(s) found with defined "find" criteria!');
-      res.send();
-    }
-  });
-});
+app.get('/', getIndex);
+app.get('/projects', getProjects);
+app.post('/project/add/:name', addProject);
+app.post('/project/remove/:name', removeProject);
+app.get('/:project/apis', getApis);
+app.post('/:project/api/add', addApi);
+app.post('/:project/api/edit/:id', editApi);
+app.post('/:project/api/delete/:id', deleteApi);
+app.post('/api/sync', syncApi);
+
+//generated endpoints
+app.get('/:project/*', simpleResponse);
+app.post('/:project/*', simpleResponse);
+app.put('/:project/*', simpleResponse);
+app.delete('/:project/*', simpleResponse);
+app.patch('/:project/*', simpleResponse);
+app.copy('/:project/*', simpleResponse);
+app.head('/:project/*', simpleResponse);
+app.options('/:project/*', simpleResponse);
+app.link('/:project/*', simpleResponse);
+app.unlink('/:project/*', simpleResponse);
+app.purge('/:project/*', simpleResponse);
+
 
 app.listen(PORT, function () {
   console.log('Example app listening on port 1234!');
@@ -208,85 +224,5 @@ app.listen(PORT, function () {
   MongoClient.connect(url, function (err, db) {
     console.log("Connected correctly to server.");
     mongoDBInstance = db;
-    // db.collection('test1').insertOne({
-    //   "name": "hi",
-    //   "list": [1,2]
-    // }, function(err, result){
-    //   console.log("error: ", err);
-    //   console.log("Inserted a document into the restaurants collection.");
-    // });
-    // db.close();
   });
 });
-
-
-//
-// function handleRequest(request, response) {
-//
-//   try {
-//     dispatcher.dispatch(request, response);
-//
-//     dispatcher.setStatic('resources');
-//
-//     dispatcher.onGet("/", function (req, res) {
-//       res.writeHead(200, {'Content-Type': 'text/html'});
-//       res.write(indexTemplate);
-//       res.end();
-//     });
-//
-//     dispatcher.onGet("/apis", function (req, res) {
-//       res.writeHead(200, {'Content-Type': 'application/json'});
-//       mongoDBInstance.collection('apis').find().toArray(function(err, result){
-//         if (err) {
-//           console.log(err);
-//         } else if (result.length) {
-//           var final = {
-//             "apis": result
-//           };
-//           console.log('Found:', result);
-//         } else {
-//           console.log('No document(s) found with defined "find" criteria!');
-//         }
-//         res.write(JSON.stringify(final));
-//         res.end();
-//       });
-//
-//     });
-//
-//     dispatcher.onPost("/post1", function (req, res) {
-//       res.writeHead(200, {'Content-Type': 'text/plain'});
-//       res.end('Got Post Data');
-//     });
-//
-//   } catch (err) {
-//     console.log(err);
-//   }
-//
-// }
-//
-// fs.readFile('./index.html', function(err, html){
-//
-//   indexTemplate = html;
-//
-//   server = http.createServer(handleRequest);
-//
-//   server.listen(PORT, function () {
-//     console.log("Server listening on: http://localhost:%s", PORT);
-//
-//     var url = 'mongodb://localhost:27017/olpdummy';
-//     MongoClient.connect(url, function(err, db) {
-//       console.log("Connected correctly to server.");
-//       mongoDBInstance = db;
-//       // db.collection('test1').insertOne({
-//       //   "name": "hi",
-//       //   "list": [1,2]
-//       // }, function(err, result){
-//       //   console.log("error: ", err);
-//       //   console.log("Inserted a document into the restaurants collection.");
-//       // });
-//       // db.close();
-//     });
-//
-//   });
-//
-// });
